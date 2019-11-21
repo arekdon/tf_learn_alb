6a15e588e7cde65bcbca098fa6e5686bccfd1565
Arek Donajski <arkadiusz.donajski@contracted.pmi.com>
2019-11-21 18:32:09 +0100

    adding valid outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-output "aws_cidr_block" {
-  value = aws_vpc.main.cidr_block
+output "aws_lb_id" {
+  value = aws_lb.v2lb.id
+}
+
+output "aws_lb_arn" {
+  value = aws_lb.v2lb.arn
+}
+
+output "aws_lb_dns_name" {
+  value = aws_lb.v2lb.dns_name
 }
